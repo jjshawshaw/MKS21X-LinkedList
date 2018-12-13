08a4d32c903b83d75b430162f2cd6dd3d4e45766
jjshawshaw <jshaw00@stuy.edu>
2018-12-13 14:38:05 -0500

    added Node methods

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -4,6 +4,34 @@ class MyLinkedList{
   private class Node{
    private int data;
    private Node next,prev;
+
+   public Node(int datain, Node nextin, Node previn){
+     data = datain;
+     next = nextin;
+     prev = previn;
+   }
+
+    public Node next(){
+      return next;
+    }
+    public Node prev(){
+      return prev;
+    }
+    void setNext(Node other){
+      next = other;
+    }
+    void setPrev(Node other){
+      prev = other;
+    }
+    Integer getData(){
+      return data;
+    }
+    Integer setData(Integer i){
+      data = i;
+    }
+    String toString(){
+      return "" + data;
+    }
   }
 
  private int size;
